339784fec6eee74b4485298504221e7fc2d9fae2
Jkhusanov <khusanov.jakhongir@gmail.com>
2019-07-12 21:31:31 -0700

    Render activity indicator

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 
 import AuthorRow from './AuthorRow';
 
 export default function Card({ fullname, image, linkText, onPressLinkText }) {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <View>
       <AuthorRow fullname={fullname} linkText={linkText} onPressLinkText={onPressLinkText} />
-      <Image style={styles.image} source={image} />
+      <View style={styles.image}>
+        {loading && <ActivityIndicator style={StyleSheet.absoluteFill} size="large" />}
+        <Image style={StyleSheet.absoluteFill} source={image} onLoad={handleLoad} />
+      </View>
     </View>
   );
 }
