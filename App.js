b21f8c158948fc0cbae2c65b43ecf21e9f6595d7
Jkhusanov <khusanov.jakhongir@gmail.com>
2019-07-21 12:23:00 -0700

    Add state variables and implement function properties

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
 import Constants from 'expo-constants';
 
 import Feed from './screens/Feed';
 
 export default function App() {
+  const [commentsForItem, setCommentsForItem] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
+
+  const openCommentScreen = id => {
+    setShowModal(true);
+    setSelectedItemId(id);
+  };
+
+  const closeCommentScreen = () => {
+    setShowModal(false);
+    setSelectedItemId(null);
+  };
+
   return (
     <View style={styles.container}>
       <Feed style={styles.feed} />
