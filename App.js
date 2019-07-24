6d29893bcd931d4c0ca77be9a39801873e2b47ba
Jkhusanov <khusanov.jakhongir@gmail.com>
2019-07-23 19:11:45 -0700

    Add storing comments to AsyncStorage

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Platform, Modal } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Platform, Modal, AsyncStorage } from 'react-native';
 import Constants from 'expo-constants';
 
 import Feed from './screens/Feed';
 import Comments from './screens/Comments';
 
+const ASYNC_STORAGE_COMMENTS_KEY = 'ASYNC_STORAGE_COMMENTS_KEY';
+
 export default function App() {
   const [commentsForItem, setCommentsForItem] = useState({});
   const [showModal, setShowModal] = useState(false);
   const [selectedItemId, setSelectedItemId] = useState(null);
+
+  useEffect(() => {
+    async function getComments() {
+      try {
+        const savedCommentsForItem = await AsyncStorage.getItem(ASYNC_STORAGE_COMMENTS_KEY);
+        setCommentsForItem(savedCommentsForItem ? JSON.parse(savedCommentsForItem) : {});
+      } catch (e) {
+        console.log('Failed to load comments');
+      }
+    }
+    getComments();
+  }, []);
 
   const openCommentScreen = id => {
     setShowModal(true);
@@ -20,13 +34,18 @@ export default function App() {
     setSelectedItemId(null);
   };
 
-  const onSubmitComment = text => {
+  const onSubmitComment = async text => {
     const comments = commentsForItem[selectedItemId] || [];
     const updated = {
       ...commentsForItem,
       [selectedItemId]: [...comments, text],
     };
     setCommentsForItem(updated);
+    try {
+      await AsyncStorage.setItem(ASYNC_STORAGE_COMMENTS_KEY, JSON.stringify(updated));
+    } catch (e) {
+      console.log('Failed to save comment', text, 'for', selectedItemId);
+    }
   };
 
   return (
